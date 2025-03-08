aee5ac5f6637e14534dcba441f3babadf64cff2f
Your Name <ranjith-devop.com>
2025-03-08 21:34:38 +0530

    new track

--- busbackend/check.js
+++
@@ -1,8 +0,0 @@
-const express = require('express')
-const app = express()
-const path = require("path")
-const PORT = process.env.PORT || 5000;
-app.get('^/$|busRoutes(.js)?',(req,res)=>{
-    res.sendFile(path.join(__dirname,'routes','busRoutes.js'));
-})
-app.listen(PORT, () => console.log(`Server running on port ${PORT}`));
---
+++ check.js
@@ -0,0 +1,19 @@
+ let num="jklmjl"
+ let mj=45.67
+ let f="653.546hg"
+let m=num=== 10
+console.log(m)
+console.log(typeof(mj))
+console.log(typeof(nu))
+console.log(num.length)
+console.log(num.charAt(1))
+console.log(num.lastIndexOf("l"))
+console.log(num.slice(3))
+console.log(num.split("l"))
+console.log(Number.parseFloat(f))
+console.log(parseFloat(f).toFixed(2).toString())
+console.log(isNaN(f))
+console.log(Number.isNaN(num))
+console.log(Math.trunc(Math.PI))
+console.log(Math.PI)
+console.log(num.charAt(Math.floor(Math.random() * num.length)));
